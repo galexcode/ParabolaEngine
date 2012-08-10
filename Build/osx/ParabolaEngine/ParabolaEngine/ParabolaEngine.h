abdaddd8338b996b1b6b1651d51ddf3f5cc2fe55
Artur Moreira <Hellhound@DevilWithin-PC.(none)>
2012-08-10 03:27:02 +0100

    OSX

---
+++ Build/osx/ParabolaEngine/ParabolaEngine/ParabolaEngine.h
@@ -0,0 +1,13 @@
+//
+//  ParabolaEngine.h
+//  ParabolaEngine
+//
+//  Created by Artur on 7/25/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParabolaEngine : NSObject
+
+@end
---
+++ Build/osx/ParabolaEngine/ParabolaEngine/ParabolaEngine.m
@@ -0,0 +1,23 @@
+//
+//  ParabolaEngine.m
+//  ParabolaEngine
+//
+//  Created by Artur on 7/25/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ParabolaEngine.h"
+
+@implementation ParabolaEngine
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
